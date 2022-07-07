e216ea7d9c91de5c8709a26529fcf3fadd91dcdc
Cpamela19 <105684139+Cpamela19@users.noreply.github.com>
2022-07-07 14:37:07 -0500

    PROYECTO
    SIPAU

---
+++ PROYECTO/script/script.js
@@ -0,0 +1,534 @@
+var users = [
+    {
+        "manzana": 4,
+        "villa": 1,
+        "cedula": "0945718519",
+        "nombre": "Luís" ,
+        "apellido": "de Ballesteros",
+        "telefono": "0937465753",
+        "correo_electronico": "jb@gmail.com",
+        "fechainicio": "27/12/2020"
+    },
+    {
+        "manzana": 4,
+        "villa": 2,
+        "cedula": "0943067232",
+        "nombre": "Juan José ",
+        "apellido": "Castro Querol",
+        "telefono": "0938588834",
+        "correo_electronico": "jjcq@gmail.com",
+        "fechainicio": "27/05/2021"
+    },
+    {
+        "manzana": 4,
+        "villa": 3,
+        "cedula": "0957151032",
+        "nombre": "Eloísa",
+        "apellido": " Pons Vizcaíno",
+        "telefono": "0940507421",
+        "correo_electronico": "epv@gmail.com",
+        "fechainicio": "27/11/2020"
+    },
+    {
+        "manzana": 4,
+        "villa": 4,
+        "cedula": "0936834767",
+        "nombre": "Joan Luján Zurita",
+        "apellido": "Luján Zurita",
+        "telefono": "0946861279",
+        "correo_electronico": "jlz@gmail.com",
+        "fechainicio": "17/10/2020"
+    },
+    {
+        "manzana": 4,
+        "villa": 5,
+        "cedula": "0955767825",
+        "nombre": "Marianela ",
+        "apellido": "Coloma Cifuentes",
+        "telefono": "0941819729",
+        "correo_electronico": "mcf@gmail.com",
+        "fechainicio": "22/05/2021"
+    },
+    {
+        "manzana": 4,
+        "villa": 6,
+        "cedula": "0956878318",
+        "nombre": "Julio ",
+        "apellido": "España Collaguazo",
+        "telefono": "0945407666",
+        "correo_electronico": "julio-elpro12@hotmail.com",
+        "fechainicio": "15/08/2020"
+    },
+    {
+        "manzana": 2,
+        "villa": 1,
+        "cedula": "0973653562",
+        "nombre": "Iker Gregorio ",
+        "apellido": "Carnero Ramos",
+        "telefono": "0938852595",
+        "correo_electronico": "ikerg@gmail.com",
+        "fechainicio": "15/09/2021"
+    },
+    {
+        "manzana": 2,
+        "villa": 2,
+        "cedula": "0947194375",
+        "nombre": "Emperatriz Noelia ",
+        "apellido": "Rosales Uriarte",
+        "telefono": "0945624984",
+        "correo_electronico": "noelia@gmail.com",
+        "fechainicio": "30/10/2021"
+    },
+    {
+        "manzana": 2,
+        "villa": 3,
+        "cedula": "0937550787",
+        "nombre": "Loreto",
+        "apellido": "Nicolás Redondo",
+        "telefono": "0944983983",
+        "correo_electronico": "lnr@gmail.com",
+        "fechainicio": "25/11/2020"
+    },
+    {
+        "manzana": 2,
+        "villa": 4,
+        "cedula": "0947694983",
+        "nombre": "Jacobo ",
+        "apellido": "Calderon Sales",
+        "telefono": "943346685",
+        "correo_electronico": "jacob@gmail.com",
+        "fechainicio": "25/11/2020"
+    },
+    {
+        "manzana": 2,
+        "villa": 5,
+        "cedula": "0938945977",
+        "nombre": "Segismundo ",
+        "apellido": "Sureda Armengol",
+        "telefono": "0940090625",
+        "correo_electronico": "",
+        "fechainicio": "25/11/2020"
+    },
+    {
+        "manzana": 3,
+        "villa": 1,
+        "cedula": "0951579604",
+        "nombre": "Jesusa ",
+        "apellido": "Chico Belda",
+        "telefono": "0944494020",
+        "correo_electronico": "jesus@gmail.com",
+        "fechainicio": "22/12/2021"
+    },
+    {
+        "manzana": 3,
+        "villa": 2,
+        "cedula": "0928885320",
+        "nombre": "Rodolfo ",
+        "apellido": "Cámara Cobo",
+        "telefono": "0937636225",
+        "correo_electronico": "rodolf@gmail.com",
+        "fechainicio": "13/07/2021"
+    },
+    {
+        "manzana": 3,
+        "villa": 3,
+        "cedula": "0974906360",
+        "nombre": "Sandalio",
+        "apellido": "Juliá Monreal",
+        "telefono": "941059671",
+        "correo_electronico": "juli@gmail.com",
+        "fechainicio": "12/09/2021"
+    },
+    {
+        "manzana": 3,
+        "villa": 4,
+        "cedula": "0948671389",
+        "nombre": "Julián ",
+        "apellido": "Real-Goñi",
+        "telefono": "0943929137",
+        "correo_electronico": "real@gmail.com",
+        "fechainicio": "18/12/2021"
+    },
+    {
+        "manzana": 5,
+        "villa": 1,
+        "cedula": "0960022408",
+        "nombre": "Irene ",
+        "apellido": "Rosell Solís",
+        "telefono": "0937838160",
+        "correo_electronico": "ire@gmail.com",
+        "fechainicio": "25/11/2020"
+    },
+    {
+        "manzana": 5,
+        "villa": 2,
+        "cedula": "0971860766",
+        "nombre": "Cleto Segarra ",
+        "apellido": "Baeza",
+        "telefono": "0943971411",
+        "correo_electronico": "cleto@gmail.com",
+        "fechainicio": "10/10/2020"
+    },
+    {
+        "manzana": 5,
+        "villa": 3,
+        "cedula": "0977738351",
+        "nombre": "Cecilia ",
+        "apellido": "Guerra Verdejo",
+        "telefono": "0946082080",
+        "correo_electronico": "ceci@gmail.com",
+        "fechainicio": "11/08/2021"
+    },
+    {
+        "manzana": 5,
+        "villa": 4,
+        "cedula": "0940035980",
+        "nombre": "Cebrián ",
+        "apellido": "Maestre Miralles",
+        "telefono": "0938269818",
+        "correo_electronico": "cmm@gmail.com",
+        "fechainicio": "10/10/2020"
+    },
+    {
+        "manzana": 1,
+        "villa": 1,
+        "cedula": "0947186603",
+        "nombre": "Catalina ",
+        "apellido": "Arellano",
+        "telefono": "0944805859",
+        "correo_electronico": "cataliana@gmail.com",
+        "fechainicio": "10/10/2020"
+    },
+    {
+        "manzana": 1,
+        "villa": 2,
+        "cedula": "0959560801",
+        "nombre": "Bernardita ",
+        "apellido": "Marqués Fábregas",
+        "telefono": "0938141622",
+        "correo_electronico": "bernar@gmail.com",
+        "fechainicio": "22/05/2021"
+    },
+    {
+        "manzana": 1,
+        "villa": 3,
+        "cedula": "0937253884",
+        "nombre": "Macaria ",
+        "apellido": "Rivas Alemán",
+        "telefono": "0944557390",
+        "correo_electronico": "maca@gmail.com",
+        "fechainicio": "22/05/2021"
+    },
+    {
+        "manzana": 1,
+        "villa": 4,
+        "cedula": "0936776491",
+        "nombre": "Alicia ",
+        "apellido": "Mur Montaña",
+        "telefono": "0939168350",
+        "correo_electronico": "alicia@gmail.com",
+        "fechainicio": "27/11/2020"
+    },
+    
+]
+
+var pagos = [
+    {
+        "num_tramite": 1,
+        "ano": 2022,
+        "mes": 7,
+        "fecha_pago": "29/5/2022",
+        "valor": 90,
+        "estado": "PENDIENTE",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 2,
+        "ano": 2022,
+        "mes": 5,
+        "fecha_pago": "28/4/2022",
+        "valor": 110,
+        "estado": "PAGADA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 3,
+        "ano": 2022,
+        "mes": 3,
+        "fecha_pago": "15/4/2022",
+        "valor": 100,
+        "estado": "EN MORA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 4,
+        "ano": 2022,
+        "mes": 2,
+        "fecha_pago": "10/3/2022",
+        "valor": 90,
+        "estado": "EN MORA",
+        "evidencia": ""
+    },
+
+    {
+        "num_tramite": 5,
+        "ano": 2022,
+        "mes": 1,
+        "fecha_pago": "10/1/2022",
+        "valor": 90,
+        "estado": "EN MORA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 6,
+        "ano": 2021,
+        "mes": 11,
+        "fecha_pago": "10/11/2021",
+        "valor": 90,
+        "estado": "PAGADA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 7,
+        "ano": 2021,
+        "mes": 10,
+        "fecha_pago": "20/10/2021",
+        "valor": 90,
+        "estado": "PAGADA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 8,
+        "ano": 2021,
+        "mes": 9,
+        "fecha_pago": "15/9/2021",
+        "valor": 90,
+        "estado": "PAGADA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 9,
+        "ano": 2021,
+        "mes": 8,
+        "fecha_pago": "7/3/2021",
+        "valor": 90,
+        "estado": "PAGADA",
+        "evidencia": ""
+    },
+    {
+        "num_tramite": 10,
+        "ano": 2021,
+        "mes": 7,
+        "fecha_pago": "12/3/2021",
+        "valor": 90,
+        "estado": "PAGADA",
+        "evidencia": ""
+    }
+]
+
+
+
+function iniciar() {
+    var email = document.getElementById("correo").value
+    var password = document.getElementById("contrasenia").value
+
+    console.log('Correo', email)
+    console.log('Contrasenia', password)
+
+    users.forEach(item => {
+        if (item.correo_electronico == email) {
+            document.getElementById("alerta").textContent = '';
+            document.getElementById("alerta").classList.remove('alert');
+
+            if (item.cedula == password) {
+                console.log('Has iniciado sesion')
+                localStorage.setItem('users', JSON.stringify(item));
+                console.log('Usuario:', item)
+                window.location = 'estado.html'
+
+            } else {
+                document.getElementById("alerta").textContent = 'Contraseña incorrecta';
+                document.getElementById("alerta").classList.add('alert');
+                console.log('Contraseña incorrecta')
+            }
+
+        } else {
+            document.getElementById("alerta").textContent = 'Usuario no encontrado';
+            document.getElementById("alerta").classList.add('alert');
+        }
+    });
+}
+
+function residente() {
+    var baseUrl = (window.location).href; 
+    var koopId = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+
+    console.log(koopId)
+
+    var localStor = localStorage.getItem("users")
+    var user = JSON.parse(localStor)
+
+    document.getElementById("nom").textContent = user.nombre;
+    document.getElementById("fechadeinicio").textContent = user.fechainicio;
+
+    console.log(user, 'usuario')
+
+    var div = document.getElementById('table');
+
+    for (var i = 0; i < pagos.length; i++) {
+
+        var color 
+
+        if(pagos[i].estado === 'PAGADA'){
+            color = 'verde'
+
+        }
+        if(pagos[i].estado === 'PENDIENTE'){
+            color = 'amarillo'
+        }
+
+        if(pagos[i].estado === 'EN MORA'){
+            color = 'rojo'
+        }
+
+        var row = `<tr >
+            <th>${pagos[i].ano}</th>
+            <th>${pagos[i].mes}</th>
+            <th>${pagos[i].fecha_pago}</th>
+            <th>${pagos[i].valor}</th>
+            <th><div class=${color}>${pagos[i].estado}</div></th>
+            <th><a href='evidencia_r.html?id=${pagos[i].num_tramite}'><img width='50' src='./imagenes/descarga.png' /></a></th>
+        </tr>`
+
+        div.innerHTML += row
+        
+    }
+}
+
+function evidencia_r() {
+    var baseUrl = (window.location).href; 
+    var id = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+    
+    const date = new Date()
+    date.setHours(0, 0, 0, 0)
+
+    document.getElementById("fechaa").value = date;
+
+    pagos.forEach(item => {
+        if (item.num_tramite == id) {
+            document.getElementById("numero").value = item.num_tramite;
+            let newDate = new Date(`${item.ano}-${item.mes}-01"`)
+            if(date > newDate){
+                //console.log('')
+            }
+            //console.log(date, '--', newDate)
+        }
+    });
+   
+}
+function fecha_pago() {
+    var baseUrl = (window.location).href; 
+    var id = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+    
+    const date = new Date()
+    date.setHours(0, 0, 0, 0)
+
+    document.getElementById("fechaa").value = date;
+    
+    pagos.forEach(item => {
+
+        if (item.num_tramite == id) {
+            document.getElementById("numero").value = item.num_tramite;
+            let newDate = new Date(`${item.ano}-${item.mes}-01"`)
+           
+            if(date > newDate){
+                console.log('')
+            }
+            
+            console.log(date, '--', newDate)
+           
+        }
+    });
+   
+}
+
+function administrador() {
+    var baseUrl = (window.location).href; 
+    var koopId = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+
+    console.log(koopId)
+
+    var localStor = localStorage.getItem("users")
+    var users = JSON.parse(localStor)
+
+    document.getElementById("nomb").textContent = users.nombre;
+    document.getElementById("fechadeinicio1").textContent = users.fechainicio;
+
+    console.log(users, 'usuario')
+    
+    var div = document.getElementById('table1');
+
+    for (var i = 0; i < pagos.length; i++) {
+
+        var color 
+
+        if(pagos[i].estado === 'PAGADA'){
+            color = 'verde'
+        }
+
+        if(pagos[i].estado === 'PENDIENTE'){
+            color = 'amarillo'
+        }
+
+        if(pagos[i].estado === 'EN MORA'){
+            color = 'rojo'
+        }
+
+        var row = `<tr >
+            <th>${pagos[i].ano}</th>
+            <th>${pagos[i].mes}</th>
+            <th>${pagos[i].fecha_pago}</th>
+            <th>${pagos[i].valor}</th>
+            <th><div class=${color}>${pagos[i].estado}</div></th>
+            <th><a href='verificar_a.html?id=${pagos[i].num_tramite}'><img width='50' src='./imagenes/descarga2.png' /></a></th>
+            <th><a href='evidencia_r.html?id=${pagos[i].num_tramite}'><img width='50' src='./imagenes/descarga.png' /></a></th>
+        </tr>`
+
+        div.innerHTML += row
+        //
+    }
+}
+
+function verificar_a() {
+    var baseUrl = (window.location).href; 
+    var id = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+    
+    const date = new Date()
+    date.setHours(0, 0, 0, 0)
+
+    document.getElementById("fechaa").value = date;
+    
+    pagos.forEach(item => {
+        if (item.num_tramite == id) {
+            document.getElementById("numero").value = item.num_tramite;
+            let newDate = new Date(`${item.ano}-${item.mes}-01"`)
+            if(date > newDate){
+                //console.log('')
+            }
+            //console.log(date, '--', newDate)
+        }
+    });
+   
+}
+
+function cancelar(){
+    window.location = 'residente.html'
+}
+
+function cancelar1(){
+    window.location = 'administrador.html'
+}
+
+
+
+
